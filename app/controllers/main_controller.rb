380fe59db59affc86b0440a0b615e977ac92b188
Charles Drappier <drappier.charles@gmail.com>
2012-12-14 14:12:41 +0100

    Activate rabbit MQ

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,13 +1,12 @@
 class MainController < ApplicationController
   def index
-
-    #AMQP.start("amqp://127.0.0.1:5672") do |connection|
-    #  channel = AMQP::Channel.new(connection)
-    #  exchange = channel.fanout(Rails.application.config.amqp_topic)
-    #  exchange.publish("Test publish", :routing_key=>'message') do
-    #    connection.close{EventMachine.stop}
-    #  end
-    #end
+    AMQP.start("amqp://127.0.0.1:5672") do |connection|
+      channel = AMQP::Channel.new(connection)
+      exchange = channel.fanout(Rails.application.config.amqp_topic)
+      exchange.publish("Test publish", :routing_key=>'message') do
+        connection.close{EventMachine.stop}
+      end
+    end
   end
 
   def mail
