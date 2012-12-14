603ca876e653b6bf967c43a4083cefd6dc72d4e1
Charles Drappier <charles@Asus-X75VD.(none)>
2012-12-14 09:34:26 +0100

    Init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tenfou::Application.config.secret_token = '967e1aa0b271313d79a8940ba8d5ed5986e0faab37a5933387fa47810b0655195504f5b222bfa34dbf96f4ddc27f46a611a8821f030f590bac6cecac99456236'
